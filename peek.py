528bcb10833c3c9e75aff8ef2f931e19fd8ae038
Giacomo <thejack2307@gmail.com>
2022-11-30 21:49:55 +0100

    first_look
    optimised detection of comments

--- peek.py
+++ peek.py
@@ -1,50 +1,24 @@
 import numpy as np
 import pandas as pd
 
-header={0:'Chromosome number',1:'Source',2:'Type',3:'Start',4:'End',5:'Score',6:'Strand',7:'null',8:'phase',}
+header = ['Chromosome number','Source','Type','Start','End','Score','Strand','null','phase'] #rough header
 filepath='Homo_sapiens.GRCh38.85.gff3.gz'
+
+#a deep analysis of pandas.read_csv() documentation allows to adapt the capabilities of pandas to our input data
 df = pd.read_csv(filepath,
-                compression='gzip',
-                header=None,
                 sep='\t',
-                quotechar='"',
-                #on_bad_lines='skip', #this is the main problem
-                skiprows=200,
-                nrows=1000, #only 100 lines, to test the script on a limited dataset
-                #lineterminator='\r',
-                engine='python'
+                compression='gzip',
+                header=None, #there is no header in our input data, we have to make it manually (next line)
+                names = header, #to give a name to each column
+                nrows = 1000, #only 100 lines, to test the script on a limited dataset
+                comment = '#' #lines that start with hashtag are considered comments; so easy in this way
                 )
 
 print(df.head())
-print("Type-", type(df))
+print("Type ", type(df))
 print(df.shape)
 
 print(df.describe()) #useless
 
-#let's give a tag to each column.
-df_renamed = df.rename(columns={0: header[0],
-                                1: header[1],
-                                2: header[2],
-                                3: header[3],
-                                4: header[4],
-                                5: header[5],
-                                6: header[6],
-                                7: header[7],
-                                8: header[8]},
-                                inplace=False)
-
-print(df_renamed.head())
-
-#there are a lot of lines that start with #
-""" for i in df_renamed[header[0]]:
-    if '#' in i:
-        print(i) """
-#remove lines that start with ###
-index_bad = df_renamed['###' == df_renamed[header[0]]].index
-print(index_bad)
-df_renamed.drop(index_bad, inplace=True)
-print(df_renamed.head()) #58245 righe nulle
-
-for i in df_renamed[header[0]]:
-    if '#' in i:
-        print(i)
+for i in header:
+    print(df[i])
