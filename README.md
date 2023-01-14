280934db2ea4e2faf0a163181922c132bf633d77
Giacomo <thejack2307@gmail.com>
2023-01-14 12:11:09 +0100

    column types

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Advanced programming project - Genomics 2023
 final project of the advanced programming classes, course of Genomics bachelor degree - UniBo.
 
-you can find the project specification [here](Project_specification.md)
+you can find the project specification [here](MarkdownFiles/Project_specification.md)
 
-you can find the documentation [here](Documentation.md)
+you can find the documentation [here](MarkdownFiles/Documentation.md)
--- peek.py
+++ peek.py
@@ -10,8 +10,9 @@ df = pd.read_csv(filepath,
                 compression='gzip',
                 header=None, #there is no header in our input data, we have to make it manually (next line)
                 names = header, #to give a name to each column
-                nrows = 100, #only 100 lines, to test the script on a limited dataset
-                comment = '#' #lines that start with hashtag are considered comments; so easy in this way
+                #nrows = 100, #only 100 lines, to test the script on a limited dataset
+                comment = '#', #lines that start with hashtag are considered comments; so easy in this way
+                na_values = '.'
                 )
 
 print(df.head())
@@ -28,18 +29,22 @@ for i in header:
 #to figure out the type of each column
 #we retrive the set of all the possible elements of each column (for some of them they will be many)
 elements_of_columns = {}
+count_na=0
 for i in header:
     if i not in ['Start','End','Attribute','Score']:
         for value in df[i].values:
-            if i in elements_of_columns:
-                if value not in elements_of_columns[i]:
-                    elements_of_columns[i].append(value)
-                else: pass
+            if not pd.isna(value):
+                if i in elements_of_columns:
+                    if value not in elements_of_columns[i]:
+                        elements_of_columns[i].append(value)
+                    else: pass
+                else:
+                    elements_of_columns[i] = [value]
             else:
-                elements_of_columns[i] = [value]
+                count_na += 1
 for index,list in elements_of_columns.items():
     print(index,list)
-
+print(count_na)
 """
 Type ['chromosome', 'biological_region', 'gene', 'processed_transcript', 'exon', 'pseudogenic_transcript', 'pseudogene', 'miRNA_gene', 'miRNA', 'lincRNA_gene', 'lincRNA', 'transcript', 'CDS', 'processed_pseudogene', 'snRNA_gene', 'snRNA', 'five_prime_UTR', 'three_prime_UTR', 'aberrant_processed_transcript', 'NMD_transcript_variant', 'RNA', 'snoRNA_gene', 'snoRNA', 'rRNA_gene', 'rRNA', 'V_gene_segment', 'C_gene_segment', 'J_gene_segment', 'VD_gene_segment', 'supercontig', 'mt_gene']
 Source ['GRCh38', '.', 'havana', 'mirbase', 'ensembl_havana', 'ensembl', 'insdc']
