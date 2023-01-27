3d88d0418df253014bd71779db50bbefe7541f76
Giacomo <thejack2307@gmail.com>
2023-01-27 09:17:19 +0100

    test

--- webapp/index.py
+++ webapp/index.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/test')
+def test():
+    return 'test'
+
 #if __name__=='__main__':
 #    app.run(debug = True)
