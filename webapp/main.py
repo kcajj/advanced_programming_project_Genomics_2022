485e93df78d21ba50b4091d1650b11750d844a54
Giacomo <thejack2307@gmail.com>
2023-02-01 18:16:08 +0100

    stable version of interface

--- webapp/main.py
+++ webapp/main.py
@@ -1,25 +1,30 @@
 from flask import Flask, render_template
-  
+from dataset_reader import GFF3DatasetReader
+
+# reading the human genome
+filepath='Homo_sapiens.GRCh38.85.gff3.gz'
+reader = GFF3DatasetReader()
+human_genome = reader.read(filepath)
+
 # Setting up the application
 app = Flask(__name__, static_folder='static')
 
 # making route
 @app.route('/')
 def home():
-    return render_template('Homepage.html')
+    return render_template('homepage.html')
 
-@app.route('/activeoperations')
+@app.route('/active_operations')
 def active_operations():
-    return render_template('actops.html')
+    return render_template('active_operations.html')
 
-@app.route('/operations')
-def operations():
-    return render_template('ops.html')
+@app.route('/operation')
+def operation():
+    return render_template('operation.html')
 
-@app.route('/projectdocumentation')
-def proj_documentation():
-    return render_template('projdoc.html')
-  
+@app.route('/documentation')
+def documentation():
+    return render_template('documentation.html')
   
 # running application
 if __name__ == '__main__':
