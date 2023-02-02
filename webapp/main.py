bd8f7417ee04f1027175891bcb47bad9bb5da0c7
Giacomo <thejack2307@gmail.com>
2023-02-02 12:24:27 +0100

    GFF3 operations redirect to active operations

--- webapp/main.py
+++ webapp/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, redirect, url_for, make_response
 import io
 from dataset_reader import GFF3DatasetReader
 from dataset import *
@@ -7,9 +7,9 @@ filepath='Homo_sapiens.GRCh38.85.gff3.gz'
 reader = GFF3DatasetReader()
 human_genome = reader.read(filepath)
 
-global global_active_op, fixed_active_op
+global global_active_op, current_dataset
 global_active_op = human_genome.get_active_operations()
-fixed_active_op = global_active_op
+current_dataset = 'human_genome'
 
 datasets = {'human_genome': human_genome,
             'get_chromosomes': human_genome.get_chromosomes(),
@@ -23,25 +23,28 @@ app = Flask(__name__, static_folder='static')
 def homepage():
     return render_template('homepage.html')
 
-@app.route('/active_operations/human_genome')
-def active_operations():
-    global global_active_op, fixed_active_op
-    global_active_op = fixed_active_op
-    return render_template('active_operations.html', active_op = global_active_op, dataset = human_genome, dataset_name = 'human_genome')
+@app.route('/active_operations/<dataset_name>')
+def active_operations(dataset_name):
+    global global_active_op, current_dataset
 
-@app.route('/operation/<operation_name>')
-def operation(operation_name):
+    if current_dataset != dataset_name:
+        global_active_op = datasets[dataset_name].get_active_operations()
+        current_dataset = dataset_name
+
+    return render_template('active_operations.html', active_op = global_active_op, dataset = datasets[dataset_name], dataset_name = dataset_name)
+
+@app.route('/operation/<dataset_name>/<operation_name>')
+def operation(dataset_name,operation_name):
     global global_active_op
 
     output = global_active_op[operation_name]()
 
     if type(output) == Dataset:
         df = output.get_df()
-        return render_template('operation.html', operation_name = operation_name, df = df)
+        return render_template('operation.html', operation_name = operation_name, df = df, dataset_name = dataset_name)
     
     else:
-        global_active_op = output.get_active_operations()
-        return render_template('active_operations.html', active_op = global_active_op, dataset = output, dataset_name = operation_name)
+        return redirect(url_for('active_operations', dataset_name = operation_name))
 
 @app.route('/documentation')
 def documentation():
@@ -54,13 +57,18 @@ def about_us():
 @app.route('/GFF3view/<dataset_name>')
 def GFF3view(dataset_name):
     df = datasets[dataset_name].get_df()
-    return render_template('GFF3view.html', dataset_name = dataset_name, dataset=df)
+    return render_template('GFF3view.html', dataset_name = dataset_name, dataset = df)
 
-@app.route('/download/<dataset_name>')
-def download(dataset_name): #fatto con chatgpt, c'è da controllare un minimo la documentazione e capire
+@app.route('/download/<dataset_name>/<operation_name>')
+def download(dataset_name,operation_name): #fatto con chatgpt, c'è da controllare un minimo la documentazione e capire
                             #la funzione make response e io.StringIO
                             #se è roba troppo complicata concelliamo tutto
-    df = datasets[dataset_name].get_df()
+    global global_active_op
+
+    if operation_name == '':
+        df = datasets[dataset_name].get_df()
+    else:
+        df = global_active_op[operation_name]().get_df()
     # Create a buffer
     buffer = io.StringIO()
     
@@ -73,7 +81,7 @@ def download(dataset_name): #fatto con chatgpt, c'è da controllare un minimo la
     
     # Create a response object with the content-type and content-disposition headers
     response = make_response(csv_data)
-    response.headers["Content-Disposition"] = f"attachment; filename = {dataset_name}.csv"
+    response.headers["Content-Disposition"] = f"attachment; filename = {dataset_name} - {operation_name}.csv"
     response.headers["Content-type"] = "text/csv"
     
     return response
